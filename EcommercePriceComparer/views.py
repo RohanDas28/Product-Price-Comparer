ab22ff63b32e645be1be199d5dbab29e76f9e5b8
scorcism <abhishekpathak1720@gmail.com>
2021-07-19 19:53:36 +0530

    #7 linked all templtes with views

--- EcommercePriceComparer/views.py
+++ EcommercePriceComparer/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def results(request):
+    return render(request, 'results.html')
+
+def services(request):
+    return render(request, 'services.html')
