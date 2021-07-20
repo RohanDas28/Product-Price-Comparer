624802ba3bc6288f436abda5b1b97ca5698eb2dd
scorcism <abhishekpathak1720@gmail.com>
2021-07-20 12:53:05 +0530

    database fiels

--- EcommercePriceComparer/models.py
+++ EcommercePriceComparer/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    email = models.EmailField()
+    sr = models.AutoField
+    email = models.CharField(max_length=25)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=13)
     desc = models.CharField(max_length=800)
