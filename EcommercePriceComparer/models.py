f2cdb0ce980018ee64886d43db87cd647bbe043c
scorcism <abhishekpathak1720@gmail.com>
2021-07-20 12:10:30 +0530

    created model

--- EcommercePriceComparer/models.py
+++ EcommercePriceComparer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13)
+    desc = models.CharField(max_length=800)
+
+    def __str__(self):
+        return self.name
