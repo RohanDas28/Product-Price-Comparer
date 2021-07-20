404f7da7cc877e541245ee9f925d6b0b99c6399e
scorcism <abhishekpathak1720@gmail.com>
2021-07-20 12:58:00 +0530

    admin title Changes

--- EcommercePriceTracker/urls.py
+++ EcommercePriceTracker/urls.py
@@ -13,8 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+
+admin.site.site_header = "Ecommerce Tracker"
+admin.site.site_title = "Admin Portal"
+admin.site.index_title = "Ecommerce Tracker AR"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
